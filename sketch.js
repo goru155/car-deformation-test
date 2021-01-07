14d2bec073c385d96ca21229cb8d91584989903f
goru155 <74966689+goru155@users.noreply.github.com>
2021-01-07 20:56:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup() {
 function draw() {
   background(0);
   car.velocityX = speed;
-  if(wall.x-car.x < (car.width+wall.width)/2)
+  if(wall.x+car.x < (car.width+wall.width)/2)
   {
   	car.velocityX=0;
   	var deformation=0.5 * weight * speed* speed/22500;
