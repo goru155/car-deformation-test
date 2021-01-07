32db7fd2283d4cd4dabefa242f51d544997d74f3
goru155 <74966689+goru155@users.noreply.github.com>
2021-01-07 20:53:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,47 @@
 var car,wall;
-var speed,weight;
+
+
+var speed, weight; 
+
 
 function setup() {
-  createCanvas(900,400);
+  createCanvas(1600, 400);
 
-  car=createSprite(50,200,50,50);
-  wall=createSprite(800,200,20,100);
-  wall.shapeColor="grey"
+	speed=random(55,90)
+	weight=random(400,1500)
 
-  speed=random(55,90);
-  weight=random(400,1500);
+	car=createSprite(50, 200, 50,50);   
 
+	car.shapeColor=color(255);
+  
+  	wall=createSprite(1500,200, 30, height/2)
+  	wall.shapeColor=color(80,80,80)
 }
 
+
 function draw() {
-  background("black");
+  background(0);
+  car.velocityX = speed;
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+  	car.velocityX=0;
+  	var deformation=0.5 * weight * speed* speed/22500;
+	if(deformation>180)
+	{
+		car.shapeColor=color(255,0,0);
+	}
 
-  car.velocityX=speed;
+	if(deformation<180 && deformation>100)
+	{
+		car.shapeColor=color(230,230,0);
+	}
 
-  if(wall.x-car.x < (car.width=wall.width)/2){
-    car.velocityX=0;
-
-    var  deformation=0.5*width*speed*sped/22500;
-    if(deformation > 180){
-      car.shapeColor="red";
-    }
-    if(deformation < 180 && deformation > 100){
-      car.shapeColor="yellow";0
-    }
-    if(deformation < 100){
-      car.shapeColor="green";
-    }
-  }
-
+	if(deformation<100)
+	{
+		car.shapeColor=color(0,255,0);
+	}
+  }  
+  
   drawSprites();
+ 
 }
